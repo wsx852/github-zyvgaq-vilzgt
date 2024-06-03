5014f2c4ae3f720b6fdc3fdb0c38faf36bae2985
wsx852 <wsx852@gmail.com>
2024-06-03 15:57:09 +0800

    configure github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wsx852.github.io',
-  base: '/github-zyvgaq-vilzgt',
   integrations: [preact()]
 });
